e730ab6e368a5e2fb383c31d21670ea6cc7b7de3
이재원 <dlwodnjs724@naver.com>
2020-05-24 00:19:09 +0900

    #9.9 Deleting To Dos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useReducer, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 const initialState = {
   toDos: [],
 };
 
 const ADD = 'add';
+const DEL = 'del';
 
 const reducer = (state, action) => {
   switch (action.type) {
     case ADD:
-      return { toDos: [...state.toDos, { text: action.payload }] };
+      return { toDos: [...state.toDos, { text: action.payload, id: uuid() }] };
+    case DEL:
+      return {
+        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
+      };
     default:
       return;
   }
@@ -21,6 +27,7 @@ function App() {
   const onSubmit = (e) => {
     e.preventDefault();
     dispatch({ type: ADD, payload: newToDo });
+    setNewToDo('');
   };
   const onChange = (e) => {
     const {
@@ -30,7 +37,6 @@ function App() {
   };
   return (
     <>
-      <h1>Add to do</h1>
       <form onSubmit={onSubmit}>
         <input
           value={newToDo}
@@ -41,8 +47,13 @@ function App() {
       </form>
       <ul>
         <h2>To Dos</h2>
-        {state.toDos.map((toDo, index) => (
-          <li key={index}>{toDo.text}</li>
+        {state.toDos.map((toDo) => (
+          <li key={toDo.id}>
+            <span>{toDo.text}</span>
+            <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+              X
+            </button>
+          </li>
         ))}
       </ul>
     </>
