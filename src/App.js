4d57f8972a030fe34d96e6eecaced7607eea8f34
이재원 <dlwodnjs724@naver.com>
2020-05-23 23:59:40 +0900

    #9.8 Adding To Dos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 
-const INCREMENT = 'increment';
-const DECREMENT = 'decrement';
+const initialState = {
+  toDos: [],
+};
+
+const ADD = 'add';
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
+    case ADD:
+      return { toDos: [...state.toDos, { text: action.payload }] };
     default:
-      throw new Error();
+      return;
   }
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewToDo(value);
+  };
   return (
     <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
-      <button onClick={() => dispatch({ type: DECREMENT })}>Remove</button>
+      <h1>Add to do</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newToDo}
+          type="text"
+          placeholder="Write to do"
+          onChange={onChange}
+        />
+      </form>
+      <ul>
+        <h2>To Dos</h2>
+        {state.toDos.map((toDo, index) => (
+          <li key={index}>{toDo.text}</li>
+        ))}
+      </ul>
     </>
   );
 }
